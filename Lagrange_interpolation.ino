3d54a5eb9a3ef8511c715366cd1fc6dd5db08475
2wodnjs7 <64174566+2wodnjs7@users.noreply.github.com>
2021-11-24 18:33:57 +0900

    Update Lagrange_interpolation.ino

--- Lagrange_interpolation.ino
+++ Lagrange_interpolation.ino
@@ -84,8 +84,8 @@ void loop() {
   if(millis() < last_sampling_time + INTERVAL) return;
   
   float raw_dist = ir_distance();
-  //float dist_cali = 100 + 300.0 / (b - a) * (raw_dist - a);
-  float dist_cali = Lagrange_interpolation(x,y,raw_dist);
+  float dist_cali = 100 + 300.0 / (b - a) * (raw_dist - a);
+  //float dist_cali = Lagrange_interpolation(x,y,raw_dist);
 
   Serial.print("min:0,max:500,dist:");
   Serial.print(raw_dist);
